a30b98410747721e9d5ae5f2ab29d99d3bcfb5c1
Snehal-Reddy <snehalreddyk@gmail.com>
2019-01-10 08:04:57 +0530

    basic decoder

--- training_utils.py
+++ training_utils.py
@@ -33,3 +33,12 @@ def encoder_form(rnn_inputs,rnn_size,num_layers,keep_prob,sequence_list):
   single_encoder = tf.contrib.rnn.MultiRNNCell([drop_shit]*num_layers)
   a,encoder_state = tf.nn.Bdirectional_dynamic_rnn(cell_fw = single_encoder,cell_bw = single_encoder,sequence_length = sequence_list,inputs = rnn_inputs,dtype = tf.float32)
   return encoder_state
+
+def decoder_form_training_set(encoder_state, decoder_cell, decoder_embedded_input, sequence_length, decoding_scope, output_function, keep_prob, batch_size):
+    attention_states = tf.zeros([batch_size, 1, decoder_cell.output_size])
+    attention_keys, attention_values, attention_score_function, attention_construct_function = tf.contrib.seq2seq.prepare_attention(attention_states, attention_option = "bahdanau", num_units = decoder_cell.output_size)
+    training_decoder_function = tf.contrib.seq2seq.attention_decoder_fn_train(encoder_state[0],attention_keys,attention_values,attention_score_function,attention_construct_function,name = "attn_dec_train")
+    decoder_output, decoder_final_state, decoder_final_context_state = tf.contrib.seq2seq.dynamic_rnn_decoder(decoder_cell,training_decoder_function,decoder_embedded_input,sequence_length,scope = decoding_scope)
+    decoder_output_dropout = tf.nn.dropout(decoder_output, keep_prob)
+    return output_function(decoder_output_dropout)
+
